22cf04eb8a2b50ff150122b6d4c5fe2a362f8d1f
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2024-11-09 12:47:16 +0100

    Add project files.

---
+++ Polymorphism/Geometry.cs
@@ -0,0 +1,70 @@
+﻿
+namespace Polymorphism
+{   
+    //Base class Geometry
+    public class Geometry   
+    {
+        public Geometry()
+        {
+
+        }
+        //Uses virtual so we can override the base class method this also demonstrates polymorphism. 
+        public virtual double Area()
+        {
+            return Area();  
+
+        }
+    }
+    //Derived class 
+    public class Rectangle : Geometry
+    {
+        public double Width { get; set; } 
+        public double Length { get; set; } 
+        
+        public Rectangle()
+        {
+            Width = 4;
+            Length = 8;
+        }
+        //Here i use override which overrides the base method. 
+        public override double Area()
+        {
+            
+            return Width * Length; 
+            
+        }
+
+    }
+    //Derived class
+    public class Circle : Geometry
+    {
+        public double Radius { get; set; }
+       
+        public Circle()
+        {
+            Radius = 5;
+            
+        }
+        //Overrides
+        public override double Area()
+        {
+            return 3.14 * Radius * Radius; 
+        }
+    }
+    //Derived class
+    public class Square : Geometry
+    {
+        public double Length { get; set; }
+        
+        public Square()
+        {
+            Length = 5;
+            
+        }
+        //Overrides
+        public override double Area()
+        {
+            return Length * Length;
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,26 @@
+﻿//Tim Petersen SUT24
+
+namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creates object references from Geometry class that points to objects of each derived class.
+            Geometry circle = new Circle();
+            Geometry rectangle = new Rectangle();
+            Geometry square = new Square();
+
+            //Creates a list and adds the objects references that i created. 
+            List<Geometry> objects = new List<Geometry>() { circle, square, rectangle };
+                 
+            //Loops through the list and print out the name of each object and the area. This demonstrate polymorphism. 
+            foreach (Geometry area in objects)
+            {
+                Console.WriteLine($"Area of {area.GetType().Name}: {area.Area()}");               
+            }
+            
+           
+        }
+    }
+}
